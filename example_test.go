07f754d5ee02576c14a8272df820e8947d87e723
Stev Witzel <stev.witzel@gmail.com>
2014-12-27 12:52:52 -0800

    Remove unnecessary example

--- example_test.go
+++ example_test.go
@@ -18,21 +18,6 @@ func ExampleWrap() {
 	fmt.Println(err.Error())
 }
 
-func ExampleWrap_nested() {
-	// Create some foo
-	foo := &foo{}
-
-	// Recursive function call with foo.bar() at its end
-	err := nested(4, func() error {
-		return foo.bar()
-	})
-
-	// Print error
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-}
-
 func ExampleError() {
 	// Create error using convenience method
 	err := tracerr.Error("Example Error")
@@ -47,19 +32,4 @@ func ExampleErrorf() {
 
 	// Print error message and stack trace
 	fmt.Println(err.Error())
-}
-
-// Recursive function that calls the passed in function at its tail end.
-func nested(depth int, fn func() error) error {
-	if depth <= 1 {
-		return fn()
-	}
-	return nested(depth-1, fn)
-}
-
-type foo struct{}
-
-// Returns an error that has been wrapped with the current stack trace.
-func (f *foo) bar() error {
-	return tracerr.Wrap(errors.New("Example Error"))
 }
